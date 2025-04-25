316dc9f9414a4bc8346c4d6f686f448c6386245d
Velkarii <dawbor589@gmail.com>
2025-04-25 20:48:50 +0200

    Basic Movement

---
+++ Eggstasy/Source/Eggstasy/Public/GM_Eggstasy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GM_Eggstasy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EGGSTASY_API AGM_Eggstasy : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Eggstasy/Source/Eggstasy/Public/PlayerCharacter.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputMappingContext.h"
+#include "InputActionValue.h"
+#include "Camera/CameraComponent.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class EGGSTASY_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	UInputMappingContext* InputMapping;
+
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, Category = "Character")
+	UCameraComponent* Camera;
+
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+};
